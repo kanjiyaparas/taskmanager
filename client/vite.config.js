ed8a6af65c1da40375e65712724510f0826e75f3
kanjiya paras <115352713+kanjiyaparas@users.noreply.github.com>
2024-10-03 16:35:05 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       "/api": {
-        target: "http://localhost:8800",
+        target: "https://taskmanager-rsj7.onrender.com",
         changeOrigin: true,
       },
     },
