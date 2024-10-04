73f5f465c202ed3529fe13a7a2e8b882a21f9b90
kanjiya paras <115352713+kanjiyaparas@users.noreply.github.com>
2024-10-04 23:53:47 +0530

    Update apiSlice.js

--- client/src/redux/slices/apiSlice.js
+++ client/src/redux/slices/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const API_URI = import.meta.env.VITE_APP_BASE_URL;
 
-const baseQuery = fetchBaseQuery({ baseUrl: API_URI + "/api"});
+const baseQuery = fetchBaseQuery({ baseUrl: API_URI});
 
 export const apiSlice = createApi({
   baseQuery,
