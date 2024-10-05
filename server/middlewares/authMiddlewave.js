a0bfc67adf35f103ccda24f568e287ca689a0f16
kanjiya paras <115352713+kanjiyaparas@users.noreply.github.com>
2024-10-05 21:45:48 +0530

    Update authMiddlewave.js

--- server/middlewares/authMiddlewave.js
+++ server/middlewares/authMiddlewave.js
@@ -3,7 +3,7 @@ import User from "../models/user.js";
 
 const protectRoute = async (req, res, next) => {
   try {
-    let token = req.cookies?.token;
+    let token = req.headers?.token;
 
     if (token) {
       // const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
